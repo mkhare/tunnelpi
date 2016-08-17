5e67b2e2d76ae2c34b0ac2ba42b97116d5d027a7
amit chahar <amitchahar_12277@aitpune.edu.in>
2016-08-17 18:49:43 +0530

    eurkAPI key updated

--- pubnub-gateway/proj_config.js
+++ pubnub-gateway/proj_config.js
@@ -7,7 +7,7 @@ module.exports = {
 		publish_key : 'pub-c-b032ad96-906e-4a98-94b9-4b5e76ccd4e2',
 		subscribe_key : 'sub-c-c90fa6ea-38a2-11e6-bbf4-0619f8945a4f',
 		channel_name : 'hello',
-		eurekapi_key : 'SAKA79D34BK8N3JCJ8DZ',
+		eurekapi_key : 'SAK8U53V7Z2SEJ5L6DTZ',
 		serverport : 9999
 	}
 }
